c9a2796657bbda1f3eb8f1503f1f94fb2501a587
suminhong02 <sumin.hong@ssfs.org>
2019-10-21 12:05:20 -0400

    Completed NumericComparisons

--- src/NumericComparisons.java
+++ src/NumericComparisons.java
@@ -11,31 +11,11 @@ public class NumericComparisons {
         Scanner scan = new Scanner(System.in);
         int num1 = scan.nextInt();
         System.out.println(num1 + " is odd: "+ isOdd(num1));
-
-        System.out.print("Enter the number: ");
-        Scanner scan1 = new Scanner(System.in);
-        int num2 = scan1.nextInt();
-        System.out.println(num2 + " is zero: "+ isZero(num2));
-
-        System.out.print("Enter the number: ");
-        Scanner scan2 = new Scanner(System.in);
-        int num3 = scan1.nextInt();
-        System.out.println(num3 + " is positive: "+ isPositive(num3));
-
-        System.out.print("Enter the number: ");
-        Scanner scan3 = new Scanner(System.in);
-        int num4 = scan1.nextInt();
-        System.out.println(num4 + " is negative: "+ isNegative(num4));
-
-        System.out.print("Enter the number: ");
-        Scanner scan4 = new Scanner(System.in);
-        int num5 = scan1.nextInt();
-        System.out.println(num5 + " is non negative: "+ isNonNegative(num5));
-
-        System.out.print("Enter the number: ");
-        Scanner scan5 = new Scanner(System.in);
-        int num6 = scan1.nextInt();
-        System.out.println(num6 + " is non positive: "+ isNonPositive(num6));
+        System.out.println(num1 + " is zero: "+ isZero(num1));
+        System.out.println(num1 + " is positive: "+ isPositive(num1));
+        System.out.println(num1 + " is negative: "+ isNegative(num1));
+        System.out.println(num1 + " is non negative: "+ isNonNegative(num1));
+        System.out.println(num1 + " is non positive: "+ isNonPositive(num1));
     }
 
     public static boolean isZero(int num){
