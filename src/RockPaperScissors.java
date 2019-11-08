26099448d766019c3e9a60cd595f153297a9912a
suminhong02 <sumin.hong@ssfs.org>
2019-11-08 11:20:34 -0500

    Finishing RockPaperScissors

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -14,19 +14,56 @@ public class RockPaperScissors {
     public static String getComputerChoice(){
         int computer = (int)(Math.random()*3)+1;
         if (computer == 1){
-            return "Rock";
+            return "rock";
         }
         if (computer == 2){
-            return "Paper";
+            return "paper";
         }
         else{
-            return "Scissors";
+            return "scissors";
         }
 
     }
 
     public static String WhoWins(String computer, String person){
-        if()
-    }
 
+        if(computer.equals("rock") && person.equals("rock")){
+            return "You tied!";
+        }
+        else if(computer.equals("rock") && person.equals("paper")){
+            return "You win!";
+        }
+        else if(computer.equals("rock") && person.equals("scissors")){
+            return "Computer wins!";
+        }
+        else if(computer.equals("paper") && person.equals("rock")){
+            return "Computer wins!";
+        }
+        else if(computer.equals("paper") && person.equals("paper")){
+            return "You tied!";
+        }
+        else if(computer.equals("paper") && person.equals("scissors")){
+            return "You win!";
+        }
+        else if(computer.equals("scissors") && person.equals("rock")){
+            return "You win!";
+        }
+        else if(computer.equals("scissors") && person.equals("paper")){
+            return "Computer wins!";
+        }
+        else{
+            return "You tied!";
+        }
+    }
+    public static void main(String [] args){
+
+
+        System.out.println("Please choose either rock, paper, or scissors.");
+        String person = getUserChoice();
+        String computer = getComputerChoice();
+        System.out.println("You chose " +person+ ".");
+        System.out.println("The computer chose " + computer + ".");
+        String winner = WhoWins(computer, person);
+        System.out.println(winner);
+    }
 }
